91ce084a0b6e3b7cdd8bcec11ce5eaf075afd1a7
persue123 <1220438725@qq.com>
2017-08-18 14:19:48 +0800

    111

--- entry/modules/ShowProject.jsx
+++ entry/modules/ShowProject.jsx
@@ -146,6 +146,12 @@ var ShowProject = React.createClass({
             }.bind(this)
         );
     },
+    groupRegistration:function (item) {
+        this.setState({modal:item});
+        var successModal = this.refs['successModal'];
+        $(successModal).modal('show');
+
+    },
     render:function() {
         var contains = null;
         var doSignupPerson = this.doSignupPerson;
@@ -169,7 +175,7 @@ var ShowProject = React.createClass({
                         <td>{item.maxTeamPersonNum}</td>
                         <td>
                             <span style={{fontSize:'16px',borderRadius:'2px'}}>
-                                <button className="search-Btn" style={{borderRadius:'3px'}} >编辑队伍</button>
+                                <button className="search-Btn" style={{borderRadius:'3px'}} onClick={ref.groupRegistration}>编辑队伍</button>
                              </span>
                         </td>
                         <td>
@@ -238,7 +244,32 @@ var ShowProject = React.createClass({
                         <div className="clearfix"></div>
                     </div>
                 </div>
+                <div className="modal fade bs-example-modal-sm login-container"
+                     tabIndex="-1"
+                     role="dialog"
+                     aria-labelledby="myLargeModalLabel"
+                     aria-hidden="true"
+                     ref='successModal'
+                     data-keyboard="false"
+                     style={{zIndex: 1045}}
+                    >
+                    <div className="modal-dialog modal-sm"
+                         style={{position: 'absolute', top: '30%', width: '50%', marginLeft: '25%'}}>
+                        <div className="modal-content"
+                             style={{position: 'relative', width: '100%', padding: '40px'}}>
+                            <div className="modal-body">
+                                <div className="modalEventDetail">
+                                     sdsds
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
             </div>
+
 
         return contains
 
